bd101d019ef5461f9389301e5136759fd6b8f806
Camilo Santander <camilosantander@usantotomas.edu.co>
2023-01-22 21:17:30 -0500

    Logica de los comandos C,U,D

--- main.py
+++ main.py
@@ -3,22 +3,60 @@
 
 clients = "pablo,ricardo,"
 
+def _get_client_name():
+    return input("What is the client\'s name?")
+
 def create_client(client_name):
     global clients
-    clients+=client_name
-    _add_comma()
+    if client_name not in clients:
+        clients+=client_name
+        _add_comma()
+    else: 
+        print("Client already exists")
     
 def _add_comma(): 
     global clients
     clients += ","
     
 
+def update_client(client_name, updated_client_name):
+    global clients
+    if client_name  in clients:
+        clients = clients.replace(client_name + ",", updated_client_name + ",")
+    else:
+        print("Client is not in client's list")
+
 def list_clients():
     global clients
     print(clients)
     
+    
+def _print_welcome():
+    print("Welcome to Ventas")
+    print("*"*50)
+    print("What would you like to do today?")
+    print("[C]reate client")
+    print("[U]pdate client")
+    print("[D]elete client")
+    
 if __name__ == "__main__":
-    list_clients()
-    create_client("david")
-    list_clients()
+    _print_welcome()
+    
+    command = input().upper()
+    
+    if command == "C":
+        client_name = _get_client_name()
+        create_client(client_name)
+        list_clients()
+    elif command == "D":
+        pass
+    elif command == "U":
+        client_name = _get_client_name()
+        updated_client_name = input("What is the updated client's name?")
+        update_client(client_name, updated_client_name)
+        list_clients()
+    else:
+        print("Invalid command")
+    
+
 
